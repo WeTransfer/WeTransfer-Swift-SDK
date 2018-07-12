523ff3ad2db4efc13e2607c8c5071d7713e5ef05
Pim Coumans <pim@pixelrock.nl>
2018-07-12 12:47:08 +0200

    Restructure of Endpoints.swift, added documentation to all endpoints

--- WeTransfer/Server/Endpoints/Endpoints.swift
+++ WeTransfer/Server/Endpoints/Endpoints.swift
@@ -8,17 +8,54 @@
 
 import Foundation
 
+extension APIEndpoint {
+	/// Sends the API key to authorize the client to use the API
+	///
+	/// - Returns: APIEndpoint with `POST` to `/authorize` without a token in the headers, expecting a `AuthorizerResponse` as response
+	static func authorize() -> APIEndpoint<AuthorizeResponse> {
+		return APIEndpoint<AuthorizeResponse>(method: .post, path: "authorize", requiresAuthentication: false)
+	}
+	
+	/// Creates a new transfer
+	///
+	/// - Returns: APIEndpoint with `POST` to `/transfer`, expecting a `CreateTransferResponse` as response
+	static func createTransfer() -> APIEndpoint<CreateTransferResponse> {
+		return APIEndpoint<CreateTransferResponse>(method: .post, path: "transfers")
+	}
+	
+	/// Adds files to an existing transfer
+	///
+	/// - Parameter transferIdentifier: Identifier of the transfer to add the files to
+	/// - Returns: APIEndpoint with `POST` to `/transfers/{id}/items`, expecting an array of `AddFilesResponse` structs as response
+	static func addItems(transferIdentifier: String) -> APIEndpoint<[AddFilesResponse]> {
+		return APIEndpoint<[AddFilesResponse]>(method: .post, path: "transfers/\(transferIdentifier)/items")
+	}
+	
+	/// Requests upload info of a chunk of a file to be uploaded
+	///
+	/// - Parameters:
+	///   - fileIdentifier: Identifier of the file to get the chunk info of
+	///   - chunkIndex: Index of the chunk
+	///   - multipartIdentifier: Multipart identifier of the file
+	/// - Returns: APIEndpoint with `GET` to `/files/{file-id}/uploads/{chunk-number}/{multipart-id}`
+	static func requestUploadURL(fileIdentifier: String, chunkIndex: Int, multipartIdentifier: String) -> APIEndpoint<AddUploadURLResponse> {
+		let partNumber = chunkIndex + 1
+		return APIEndpoint<AddUploadURLResponse>(method: .get, path: "files/\(fileIdentifier)/uploads/\(partNumber)/\(multipartIdentifier)")
+	}
+	
+	/// Completes the upload of file, assuming all chunks have finished uploading
+	///
+	/// - Parameter fileIdentifier: Identifier of the file
+	/// - Returns: APIEndpoint with `POST` to `/files/{file-id}/uploads/complete`
+	static func completeUpload(fileIdentifier: String) -> APIEndpoint<CompleteUploadResponse> {
+		return APIEndpoint<CompleteUploadResponse>(method: .post, path: "files/\(fileIdentifier)/uploads/complete")
+	}
+}
+
 // MARK: - Authorize
 struct AuthorizeResponse: Decodable {
 	let success: Bool
 	let token: String?
-}
-
-extension APIEndpoint {
-	static func authorize() -> APIEndpoint<AuthorizeResponse> {
-		// Only request that doesn't require a jwt token to be set
-		return APIEndpoint<AuthorizeResponse>(method: .post, path: "authorize", requiresAuthentication: false)
-	}
 }
 
 // MARK: - Create transfer
@@ -35,12 +72,6 @@ struct CreateTransferParameters: Encodable {
 struct CreateTransferResponse: Decodable {
 	let id: String // swiftlint:disable:this identifier_name
 	let shortenedUrl: URL
-}
-
-extension APIEndpoint {
-	static func createTransfer() -> APIEndpoint<CreateTransferResponse> {
-		return APIEndpoint<CreateTransferResponse>(method: .post, path: "transfers")
-	}
 }
 
 // MARK: - Add files
@@ -84,12 +115,6 @@ struct AddFilesResponse: Decodable {
 	let uploadExpiresAt: TimeInterval
 }
 
-extension APIEndpoint {
-	static func addItems(transferIdentifier: String) -> APIEndpoint<[AddFilesResponse]> {
-		return APIEndpoint<[AddFilesResponse]>(method: .post, path: "transfers/\(transferIdentifier)/items")
-	}
-}
-
 // MARK: - Request upload URL
 struct AddUploadURLResponse: Decodable {
 	let uploadUrl: URL
@@ -98,21 +123,8 @@ struct AddUploadURLResponse: Decodable {
 	let uploadExpiresAt: TimeInterval
 }
 
-extension APIEndpoint {
-	static func requestUploadURL(fileIdentifier: String, chunkIndex: Int, multipartIdentifier: String) -> APIEndpoint<AddUploadURLResponse> {
-		let partNumber = chunkIndex + 1
-		return APIEndpoint<AddUploadURLResponse>(method: .get, path: "files/\(fileIdentifier)/uploads/\(partNumber)/\(multipartIdentifier)")
-	}
-}
-
 // MARK: - Complete upload
 struct CompleteUploadResponse: Decodable {
 	let ok: Bool // swiftlint:disable:this identifier_name
 	let message: String
-}
-
-extension APIEndpoint {
-	static func completeUpload(fileIdentifier: String) -> APIEndpoint<CompleteUploadResponse> {
-		return APIEndpoint<CompleteUploadResponse>(method: .post, path: "files/\(fileIdentifier)/uploads/complete")
-	}
 }
