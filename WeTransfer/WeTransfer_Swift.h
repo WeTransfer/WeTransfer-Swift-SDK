33005be586f6e222a49d9a9c37c0d138ffa5931b
Pim Coumans <pim@pixelrock.nl>
2018-07-16 11:20:23 +0200

    Seperate targets for iOS and macOS

--- WeTransfer/WeTransfer_Swift.h
+++ WeTransfer/WeTransfer_Swift.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 WeTransfer. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for WeTransfer_Swift.
 FOUNDATION_EXPORT double WeTransfer_SwiftVersionNumber;
