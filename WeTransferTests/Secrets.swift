d8d64e3cc6a1e1d2e495a5bf7ae33fbdecc4a8a9
Pim Coumans <pim@pixelrock.nl>
2018-07-18 11:31:31 +0200

    Updated Secrets to throw a fatalError for all values, no longer warns with Danger

--- WeTransferTests/Secrets.swift
+++ WeTransferTests/Secrets.swift
@@ -16,18 +16,25 @@ struct Secrets {
 		case stagingURL = "staging-url"
 	}
 	
-	// FIXME: This retrieves the API keys from a plist that is used for running tests on travis, for local testing, please manually add Secrets.plist with at least a `production` key or replace the value below with your key
+	/* API keys are retrieved from 'Secrets.plist' which is used for running tests on travis.
+	For local testing, please either manually add Secrets.plist with at least a 'production' key or replace the value below with your API key */
 	static var productionKey: String {
-		return value(for: .productionKey)
+		guard let value = value(for: .productionKey) else {
+			fatalError("Production API key not found in Secrets.plist. Provide your own Secrets.plist with '\(SecretName.productionKey.rawValue)' or manually replace variable here")
+		}
+		return value
 	}
 	
 	static var stagingKey: String {
-		return value(for: .stagingKey)
+		guard let value = value(for: .stagingKey) else {
+			fatalError("Staging API key not found in Secrets.plist. Provide your own Secrets.plist with '\(SecretName.stagingKey.rawValue)' or manually replace variable here")
+		}
+		return value
 	}
 	
 	static var stagingURL: URL {
-		guard let url = URL(string: value(for: .stagingURL)) else {
-			fatalError("Staging URL not available")
+		guard let value = value(for: .stagingURL), let url = URL(string: value) else {
+			fatalError("Staging URL not found in Secrets.plist. Provide your own Secrets.plist with '\(SecretName.stagingURL.rawValue)' or manually replace variable here")
 		}
 		return url
 	}
@@ -35,14 +42,13 @@ struct Secrets {
 
 extension Secrets {
 	private static var plistURL: URL? {
-		return Bundle(for: TestConfiguration.shared.classForCoder).url(forResource: "Secrets", withExtension: "plist")
+		return Bundle(for: TestConfiguration.shared.classForCoder).url(forResource: "Secretsu", withExtension: "plist")
 	}
 	
-	private static func value(for secret: SecretName) -> String {
-		guard let url = plistURL, let keysDictionary = NSDictionary(contentsOf: url),
-			let value = keysDictionary[secret.rawValue] as? String else {
-				fatalError("'\(secret.rawValue)' key not available, provide your own Secrets.plist or manually define the keys in the Secrets struct")
+	private static func value(for secret: SecretName) -> String? {
+		guard let url = plistURL, let keysDictionary = NSDictionary(contentsOf: url) else {
+			return nil
 		}
-		return value
+		return keysDictionary[secret.rawValue] as? String
 	}
 }
